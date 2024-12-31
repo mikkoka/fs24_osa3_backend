620c4fbdfa0a5de5de8a673522d264ecd1292b47
Mikko Kahri <mimak@iki.fi>
2024-12-31 14:15:08 +0200

    osa3 valmis

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const morgan = require('morgan')
 const app = express()
 const maxRandomInt = 999999999
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
 morgan.token('id', function getId (req) {
   if(req.body.name) return JSON.stringify(req.body)
 })
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
@@ -21,101 +25,88 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-const getRandomId = () => {
-  return Math.floor(Math.random() * maxRandomInt).toString();
-}
 
-let persons = 
-[
-    {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": "1"
-    },
-    {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": "2"
-    },
-    {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": "3"
-    },
-    {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": "4"
-    },
-    {
-    "id": "2576",
-    "name": "Mikko",
-    "number": "123123"
-    }
-]
-  
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/', (request, response, next) => {
+  response.send('<h1>Hello World!</h1>').catch(error => next(error))
 })
 
 
-app.get('/info', (request, response) => {
-  response.send(`<div>
-                  <h3>Hello World! Phonebook has info on ${persons.length} persons!</h3>
-                  <p>${Date()}</p>
-                  <p>Random int for your pleasure: ${getRandomInt()}</p>
-                </div>`)
+app.get('/info', (request, response, next) => {
+  Person.estimatedDocumentCount().then(count => {
+    response.send(`<div>
+      <h3>Hello World! Phonebook has info on ${count} persons!</h3>
+      <p>${Date()}</p>
+    </div>`)
+}).catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-  
-  response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(pers => {
+    response.json(pers)
+  }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   
-  let newPerson = request.body
+  const body = request.body
 
-  if (!newPerson.name || !newPerson.number) {
+  if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'name or number missing' 
     })
-  } 
 
-  if (persons.find((p => p.name ===  newPerson.name))) {
-    return response.status(400).json({ 
-      error: 'name has to be unique' 
+  } else {
+    newPerson = new Person({...body, number: body.number.replace(/\D/g, "-")})
+    newPerson.save().then(p => {
+      response.json(p)
+    }).catch(error => next(error))
+  }
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+
+  Person.findById(request.params.id).then(pers => {
+    console.log(pers)
+    response.json(pers)
+  })
+  .catch(error => next(error))
+
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
-  } else {
-    newPerson = {...request.body, id : getRandomId()} 
+    .catch(error => next(error))
+})
 
-    persons = persons.concat(newPerson)
-    response.json(newPerson)
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  Person.findByIdAndUpdate(request.params.id, { number: body.number.replace(/\D/g, "-") }, { new: true, runValidators: true, context: 'query'  })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-  
+  next(error)
+}
 
-})
+app.use(errorHandler)
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find((p => p.id === id))
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter((p => p.id !== id))
-
-  response.status(204).end()
-})
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://mikkoosoite:${password}@cluster0.505od.mongodb.net/puhelinluettelo?retryWrites=true&w=majority&appName=Cluster0`
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+if (process.argv.length===3) {
+    Person.find({}).then(result => {
+        result.forEach(p => {
+          console.log(p.name, p.number)
+        })
+        mongoose.connection.close()
+      })   
+  }
+
+if (process.argv.length>5) {
+    console.log('too many arguments!')
+    mongoose.connection.close()
+}
+
+if (process.argv.length===4) {
+    console.log('phone number missing')
+    mongoose.connection.close()
+}
+
+if (process.argv.length===5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    
+    person.save().then(result => {
+      console.log('person saved!')
+      mongoose.connection.close()
+    })  
+}
+
+
+
+
+
+
+
+
+
